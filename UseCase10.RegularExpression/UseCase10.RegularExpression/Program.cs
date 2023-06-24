cf5b340a03b5d173291bf644737b769e156b1d79
Halyna Leshchynska <hlias@softserveinc.com>
2023-06-24 13:45:30 +0300

    UC#10 Implement regex

---
+++ UseCase10.RegularExpression/UseCase10.RegularExpression/Program.cs
@@ -0,0 +1,16 @@
+﻿using UseCase10.RegularExpression;
+
+while (true)
+{
+	var maxLength = Console.ReadLine();
+	var input = Console.ReadLine();
+
+	if (string.IsNullOrEmpty(input) || string.IsNullOrEmpty(maxLength) || !int.TryParse(maxLength, out var maxLengthInt))
+	{
+		continue;
+	}
+
+	var result = RegularExpressionHelper.ValidateString(input, maxLengthInt);
+
+	Console.WriteLine(result);
+}
---
+++ UseCase10.RegularExpression/UseCase10.RegularExpression/RegularExpressionHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UseCase10.RegularExpression
+{
+	public static class RegularExpressionHelper
+	{
+		#region Private constants
+
+		/// <summary>
+		/// Holds Regular Expression
+		/// </summary>
+		private const string RegularExpression = @"(?=.{{1,{0}}})^(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[!""#$%&'()*+,-./:;<=>?@[\]^_`{{|}}~])\S+$";
+		
+		#endregion
+
+		/// <summary>
+		/// Validates input string by regex
+		/// </summary>
+		/// <param name="input">The input string.</param>
+		/// <param name="maxLength">The max length of input string.</param>
+		/// <returns>Result of validation. True if input string matches regex, otherwise false.</returns>
+		public static bool ValidateString(string input, int maxLength)
+		{
+			var pattern = string.Format(RegularExpression, maxLength);
+			return Regex.IsMatch(input, pattern);
+		}
+	}
+}
